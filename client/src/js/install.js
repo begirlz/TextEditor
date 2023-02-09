bf4e7fb34aa8ac452a6a943da662fdb7c0dbd1cb
Supapun Wichachuai <Supapun.Wichachuai@Microchip.com>
2023-02-09 09:18:50 -0700

    clean up comment

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,14 +1,14 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.derferredPromt = event;
 
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
 
     const promptEvent = window.deferredPrompt;
@@ -24,7 +24,7 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => { 
     // Clear prompt
   window.deferredPrompt = null;
